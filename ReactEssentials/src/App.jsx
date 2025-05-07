31248a72b066d37315c75a6a0c368da2855a6371
buildbyabhinav <121730262+buildbyabhinav@users.noreply.github.com>
2025-05-07 12:01:11 +0530

    header

--- ReactEssentials/src/App.jsx
+++ ReactEssentials/src/App.jsx
@@ -1,14 +1,24 @@
+const reactDescriptions = ['Fundamental', 'Core', 'Crucial']
+
+function getRandom(max){ return Math.floor(Math.random()*max)}
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {reactDescriptions[getRandom(3)]} React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
         <h2>Time to get started!</h2>
       </main>
