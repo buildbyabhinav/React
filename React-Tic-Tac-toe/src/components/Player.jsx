b69e89840b56332662ac6e4637aa1389b5ff5d73
buildbyabhinav <121730262+buildbyabhinav@users.noreply.github.com>
2025-05-13 13:31:01 +0530

    useState-handleChanges-functionalWayOfPassingAndTogglingBetweenValues-twoWayBinding

---
+++ React-Tic-Tac-toe/src/components/Player.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState } from "react";
+
+const Player = ({ initialName, symbol }) => {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((isEditing) => !isEditing); //pas function because function will give react past value as current and then update // using !isEditing donot immediately updates but schedules the update
+    //   setIsEditing(!isEditing)
+    // setIsEditing(!isEditing) // we expect that final result will cancel each other but that doesnt happen as execution is still in current cycle and for both of them state gets updated by same value
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  //   let btnCaption = 'Edit'
+
+  if (isEditing) {
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+    // btnCaption = 'Save'
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+};
+
+export default Player;
