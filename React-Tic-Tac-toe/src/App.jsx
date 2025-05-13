34122a839de496f2539dcf108ac78ffbfa177ca5
buildbyabhinav <121730262+buildbyabhinav@users.noreply.github.com>
2025-05-13 17:29:26 +0530

    gameboardsetup-liftingStateup-

--- React-Tic-Tac-toe/src/App.jsx
+++ React-Tic-Tac-toe/src/App.jsx
@@ -1,14 +1,20 @@
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X')
+  function handleSelectSquare(){
+    setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O' : 'X')
+  }
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName='Player1' symbol='X'/>
-          <Player initialName='Player2' symbol='O' />
+        <ol id="players" className="highlight-player">
+          <Player initialName='Player1' symbol='X' isActive={activePlayer === 'X'}/>
+          <Player initialName='Player2' symbol='O' isActive={activePlayer ==='O'}/>
         </ol>
-        GAME BOARD
+        <GameBoard onSelectSquare = {handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
       LOG
     </main>
---
+++ React-Tic-Tac-toe/src/components/GameBoard.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+const GameBoard = ({onSelectSquare, activePlayerSymbol}) => {
+
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedBoard;
+    });
+
+    onSelectSquare();
+  }
+
+  return (
+    <ol id="game-board">
+      {gameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((col, colIndex) => (
+              <li key={colIndex}>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{col}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+};
+
+export default GameBoard;
