55dfeef3af6fc9e3679e82d6bb348913efcd3a09
buildbyabhinav <121730262+buildbyabhinav@users.noreply.github.com>
2025-06-05 14:52:50 +0530

    quiz-project-setup

--- React Place Picker ( Side Effects )/src/App.jsx
+++ React Place Picker ( Side Effects )/src/App.jsx
@@ -1,15 +1,45 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from "react";
 
-import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import Places from "./components/Places.jsx";
+import { AVAILABLE_PLACES } from "./data.js";
+import Modal from "./components/Modal.jsx";
+import DeleteConfirmation from "./components/DeleteConfirmation.jsx";
+import logoImg from "./assets/logo.png";
+import { sortPlacesByDistance } from "./loc.js";
+
+const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+const storedPlaces = storedIds.map((id) =>
+  AVAILABLE_PLACES.find((place) => place.id === id)
+);
+
+// now this will run only once and not with every app execution cycle and therefore we can get our stored places
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+
+  // useEffect(() => {
+  //   const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+  //   const storedPlaces = storedIds.map((id) => {
+  //     AVAILABLE_PLACES.find((place) => place.id === id)
+  //   })
+  //   setPickedPlaces(storedPlaces)
+  // }, [])
+
+  // unline navigator, localStorage executes synchrounously rather than asynchronously and therefore executes step by step abd the return jsx after function execution and therefore useEffect is not required
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces); // this causes an infinite loop as every time the state changes the app component re executes and an infinte loops come into picture
+    }); // now this is aq side effect as it is not directly related to the JSX code to be returned but it is required to put places in an order.
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -28,6 +58,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    } // this is also a peice of side effect but we cant use and shouldnt use side effect here for first it is inside another function and a hook is used at top level of a function. second there is no requirement since it doesnt hampers the development of side in any manner third this code is executed only when user clicks on the buttons rather than on every render cycle.
   }
 
   function handleRemovePlace() {
@@ -35,6 +73,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
+    );
   }
 
   return (
@@ -57,13 +101,15 @@ function App() {
       <main>
         <Places
           title="I'd like to visit ..."
-          fallbackText={'Select the places you would like to visit below.'}
+          fallbackText={"Select the places you would like to visit below."}
           places={pickedPlaces}
           onSelectPlace={handleStartRemovePlace}
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          // places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting Places according to distance"
           onSelectPlace={handleSelectPlace}
         />
       </main>
