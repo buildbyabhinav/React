4bd7239e743b83ef5baf1f5355bd922a9803703a
buildbyabhinav <121730262+buildbyabhinav@users.noreply.github.com>
2025-06-07 16:27:40 +0530

    loading-error-handling

--- React Sending HTTP Requests/src/components/AvailablePlaces.jsx
+++ React Sending HTTP Requests/src/components/AvailablePlaces.jsx
@@ -3,19 +3,45 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('')
 
   useEffect(() => {
-    fetch("http://localhost:3000/places")
-      .then((response) => {
-        return response.json();
-      })
-      .then((resData) => setAvailablePlaces(resData.places));
+    async function fetchData() {
+      setIsLoading(true);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+        if (!response.ok) {
+          throw new Error("Fetching failed");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({message : error.message || 'Could not fetch places'})
+      }
+      setIsLoading(false);
+    }
+    fetchData();
   }, []);
+
+  if(error){
+    return <Error title="An error occured" message = {error.message}/>
+  }
+
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((resData) => setAvailablePlaces(resData.places));
+  // }, []);
 
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isLoading}
+      loadingText="Fetching Places"
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
