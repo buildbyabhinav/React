83b2be192214adfb5e3a2985cf4e136c13033ca2
buildbyabhinav <121730262+buildbyabhinav@users.noreply.github.com>
2025-06-07 22:36:05 +0530

    error-loading-setuo-of-user-places

--- React Sending HTTP Requests/src/components/AvailablePlaces.jsx
+++ React Sending HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,31 +1,44 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvailablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('')
+  const [error, setError] = useState("");
 
   useEffect(() => {
     async function fetchData() {
       setIsLoading(true);
       try {
-        const response = await fetch("http://localhost:3000/places");
-        const resData = await response.json();
-        if (!response.ok) {
-          throw new Error("Fetching failed");
-        }
-        setAvailablePlaces(resData.places);
+        const places = await fetchAvailablePlaces()
+        // const response = await fetch("http://localhost:3000/places");
+        // const resData = await response.json();
+        // if (!response.ok) {
+        //   throw new Error("Fetching failed");
+        // }
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsLoading(false); // since navigator will run sometime in future setIsLoading outside will execute first so to execute it later sometime we put it in try and cath both blocks
+        });
       } catch (error) {
-        setError({message : error.message || 'Could not fetch places'})
+        setError({ message: error.message || "Could not fetch places" });
+        setIsLoading(false);
       }
-      setIsLoading(false);
+      // setIsLoading(false);
     }
     fetchData();
   }, []);
 
-  if(error){
-    return <Error title="An error occured" message = {error.message}/>
+  if (error) {
+    return <Error title="An error occured" message={error.message} />;
   }
 
   // useEffect(() => {
